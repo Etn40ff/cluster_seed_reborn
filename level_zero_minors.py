ac1850570a30c98a07f0b1b926f9f2469a3a23d0
Harold Williams <hwilliams2718@gmail.com>
2015-09-16 16:32:22 -0700

    added generic_evaluation3 and helpers to level_zero_minors.py

--- level_zero_minors.py
+++ level_zero_minors.py
@@ -33,6 +33,7 @@ class LevelZeroMinor(SageObject):
 
         temp_coeff = []
         for i in xrange(self._rank):
+            # next two lines depend on the implementation of weights by RootSystem
             root = self._RootSystem._fundamental_weights[i]-self._coxeter_element*self._RootSystem._fundamental_weights[i]
             coeff = self._polygens[self._rank+i]**(-1)*prod([self._polygens[j]**root[self._rank+1+j] for j in xrange(self._rank)]) 
             temp_coeff.append(coeff)
@@ -80,3 +81,63 @@ class LevelZeroMinor(SageObject):
             columns = B.columns()
             source = None
         return tuple(coxeter)
+
+    def affine_weight_multiplicity(self, highest_wt, wt):
+        # return multiplicity of wt in level zero representation indexed by dominant finite-type highest_wt
+        pass
+
+    def validate_weight(self, xlist, wt1, wt2, highest_wt, alpha, pairing):
+        # check whether there is an ambiguity in the next step of generic_evaluation
+        if pairing >= 0:
+            outward_alpha = alpha
+        else:
+            outward_alpha = -alpha
+        current_wt = copy(wt1)
+        current_wt_mult = self.affine_weight_multiplicity(highest_wt, current_wt)
+        initial_wt_mult = current_wt_mult
+        while current_wt_mult != 0:
+            if current_wt_mult < initial_wt_mult:
+                print "There was an ambiguity."
+                print "initial_wt_mult = ", initial_wt_mult
+                print "current_wt_mult = ", current_wt_mult
+                print "current_wt = ", current_wt
+                print "alpha = ", alpha
+                print "xlist = ", xlist
+                print "wt1 = ", wt1
+                print "wt2 = ", wt2
+            current_wt += outward_alpha
+            current_wt_mult = self.affine_weight_multiplicity(highest_wt, current_wt)
+
+    def level_zero_dominant_conjugate(self, wt):
+        # return the dominant Weyl conjugate weight of wt
+        pass
+
+    def generic_evaluation3(self, xlist, wt1, wt2 = None, highest_wt = None):
+        if highest_wt == None:
+            highest_wt = level_zero_dominant_conjugate(wt1)
+        if wt2 == None:
+            wt2 = copy(wt1)
+        if xlist == []:
+            if wt1 == wt2:
+                return 1
+            else:
+                return 0
+        new_xlist = copy(xlist)
+        i, eps = new_xlist.pop()
+        alpha = eps * self._RootSystem._simple_roots[i]
+        pairing = self._RootSystem.pairing(alpha, wt1)
+        self.validate_weight(highest_wt, alpha, wt1, pairing)
+        output = 0
+        j = 0
+        new_wt1 = copy(wt1)
+        while self.affine_weight_multiplicity(new_wt1) != 0:
+            if eps > 0:
+                # this records the action of the matrix [[1,t],[0,1]]
+                output += self.generic_evaluation3(new_xlist, new_wt1, wt2, highest_wt) * self._polygens[i]**j
+            else:
+                # this records the action of the matrix [[u^{-1},0],[1,u]] = [[1,0],[u,1]]*[[u^{-1},0],[0,u]]
+                output += self.generic_evaluation3(new_xlist, new_wt1, wt2, highest_wt) * self._polygens[self._rank + i]**(pairing + j)
+            j += 1
+            new_wt1 += alpha
+        return output
+
